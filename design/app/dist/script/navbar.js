04ff627e01b350bb5118ed7b801f76abb1f30379
audaxss <chrono.social@outlook.com>
2021-09-11 17:32:52 +0800

    upde: mobile navigation added

--- design/app/dist/script/navbar.js
+++ design/app/dist/script/navbar.js
@@ -1,12 +1,15 @@
 const menuButton = document.querySelector('#menu-button');
 const closeButton = document.querySelector('#close-button');
 const menu = document.querySelector('.navbar__mobile');
+const body = document.body;
 const openMenu = () => {
     menu.classList.add('navbar__mobile--open');
+    body.style.overflow = 'hidden';
 }
 
 const closeMenu = () => {
     menu.classList.remove('navbar__mobile--open');
+    body.removeAttribute('style');
 }
 
 menuButton.addEventListener('click', openMenu);
